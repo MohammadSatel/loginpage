5044206af0061842f8229e074a4bb925ec045178
MohammadSatel <hamuds95@gmail.com>
2023-09-11 12:06:17 +0300

    first commit

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
 
-# Create a Flask application instance
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+# Function to check if a file has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Route to display uploaded images and handle image uploads
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        # Check if a file was uploaded
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an empty part without a filename
+        if file.filename == '':
+            return redirect(request.url)
+        # If the file is allowed, save it to the uploads folder
+        if file and allowed_file(file.filename):
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            return redirect(url_for('index'))
+
+    # List of uploaded images in the uploads folder
+    uploaded_images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', uploaded_images=uploaded_images)
+
+# Route to serve uploaded images
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
